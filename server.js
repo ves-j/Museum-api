160134bcc05391c9f75e1f4db02aa2f85e234fb4
V <vesal.jamshidi80@gmail.com>
2025-12-26 21:49:40 +0000

    Deploy as native Express on Vercel

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-import app from "./src/app.js";
+import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`API running on http://localhost:${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`API running on http://localhost:${PORT}`);
+});
